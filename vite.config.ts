f03e0c40985550b3855b76f8f27ea20e71762c0c
ICascha <caschavanwanrooij@gmail.com>
2025-06-18 09:24:56 +0200

    change node env

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/denkwerk-knooppuntenanalyse' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/denkwerk-knooppuntenanalyse/' : '/',
   plugins: [react(),     
   compression({
     algorithm: 'gzip',  // or 'brotliCompress', 'deflate', etc.
